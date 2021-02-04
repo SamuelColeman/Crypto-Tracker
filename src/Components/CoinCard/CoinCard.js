3b428fbbdbda7e34b16f98829f0e509e501160a0
Sam Coleman <scoleman@sovrn.com>
2021-02-04 16:33:54 -0700

    update card styling and information

--- src/Components/CoinCard/CoinCard.js
+++ src/Components/CoinCard/CoinCard.js
@@ -12,8 +12,16 @@ class CoinCard extends Component {
   render() {
     return (
       <div id={this.state.coinCardData.id} className="CoinCard">
-        <h1 className="coin-card-title">{this.state.coinCardData.name}</h1>
-        <h2 className="coin-card-price">${this.state.coinCardData.price_usd}</h2>
+        <p className="coin-card-rank">{this.state.coinCardData.rank}</p>
+        <div className="coin-card-name">
+          <h3 className="coin-card-symbol">{this.state.coinCardData.symbol}</h3>
+          <p className="coin-card-title">{this.state.coinCardData.name}</p>
+        </div>
+        <p className="coin-card-price">${this.state.coinCardData.price_usd}</p>
+        <p className="coin-card-percent-change-24h">{this.state.coinCardData.percent_change_24h}%</p>
+        <p className="coin-card-market-cap-usd">${this.state.coinCardData.market_cap_usd}</p>
+        <p className="coin-card-volume-24">${this.state.coinCardData.volume24}</p>
+        <p className="coin-card-c-supply">${this.state.coinCardData.csupply}</p>
       </div>
     );
   };
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,6 +5,7 @@ class NavBar extends Component {
   render() {
     return (
       <div className="NavBar">
+        <p className="nav-bar-label">Top 100 Crypto Currencies</p>
         <input className="nav-bar-search-input" placeholder="Search"></input>
       </div>
     );
